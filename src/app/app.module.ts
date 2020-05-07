d94a6d45154bc6de06af4ba30fa1b9ba07712070
Milton Honji <factorysolutionIT@hotmail.com>
2020-05-06 21:40:08 -0300

    Alterados as fontes do Home e dos Dizimistas.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { TithersModule } from './tither/tithers/tithers.module';
 import { RouterModule } from '@angular/router';
 import { HomeModule } from './pages/home/home.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -18,7 +19,8 @@ import { HomeModule } from './pages/home/home.module';
     TithersModule,
     AppRoutingModule,
     RouterModule,
-    HomeModule
+    HomeModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
