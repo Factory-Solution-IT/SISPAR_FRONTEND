def825fdbf8201b9ed8e989ee5819dc2232c70f9
Milton Honji <factorysolutionIT@hotmail.com>
2020-05-19 23:08:47 -0300

    verificando a rota do Idtither

--- src/app/tithe/tithe.service.ts
+++ src/app/tithe/tithe.service.ts
@@ -15,18 +15,22 @@ export class TitheService {
 
   getAllTithes(): Observable<Tithe[]> {
     return this.http.get<Tithe[]>(`${this.url}/api/tithes`);
+    //return this.http.get<Tithe[]>(`${this.url}/api/tithes/bytitherid/{titherId}`);
   }
 
   getTithesById(idTithe: string): Observable<Tithe[]> {
     const apiUrl = `${this.url}/api/tithes/${idTithe}`;
+    //const apiUrl = `${this.url}/api/tithes/bytitherid/{titherId}`;
     return this.http.get<Tithe[]>(apiUrl);
   }
 
   postTithe(tithe: Tithe): Observable<Tithe>{
     return this.http.post<Tithe>(`${this.url}/api/tithes`, tithe);
+    //return this.http.post<Tithe>(`${this.url}/api/tithes/bytitherid/{titherId}`, tithe);
   }
 
   deleteTithe(idTithe: string): Observable<Tithe> {
     return this.http.delete<Tithe>(`${this.url}/api/tithes/${idTithe}`);
+     //return this.http.delete<Tithe>(`${this.url}/api/tithes/bytitherid/{titherId}`);
   }
 }
