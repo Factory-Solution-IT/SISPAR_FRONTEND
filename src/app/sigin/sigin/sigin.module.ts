e80ee32cc633b4d9a3f797fca1c7d829f7227109
Milton Honji <factorysolutionIT@outlook.com>
2021-01-06 17:44:25 -0300

    Aplicando algumas coisas com materials.

--- src/app/sigin/sigin/sigin.module.ts
+++ src/app/sigin/sigin/sigin.module.ts
@@ -2,14 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SiginComponent } from './sigin.component';
 import { RouterModule } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [SiginComponent],
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    RouterModule
+    RouterModule,
+    FormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule
   ]
 })
 export class SiginModule { }
